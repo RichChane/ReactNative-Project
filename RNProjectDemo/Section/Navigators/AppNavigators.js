eab76b87277982de587af896a2537e45001b673f
Rich <chenruichen@kp99.cn>
2019-06-06 11:34:05 +0800

    react-navigatio react-native-gesture-handler

---
+++ RNProjectDemo/Section/Navigators/AppNavigators.js
@@ -0,0 +1,65 @@
+
+import {
+    Button,
+} from 'react-native';
+import {createStackNavigator} from 'react-navigation';
+
+import NaviHomePage from '../page/NaviHomePage';
+import Page1 from '../page/Page1';
+import Page2 from '../page/Page2';
+import Page3 from '../page/Page3';
+
+
+
+
+export const AppStackNavigator = createStackNavigator({
+
+    NavigationHomePage:{
+        screen:NaviHomePage
+    },
+
+    Page1:{// 动态配置 navigationOptions
+        screen:Page1,
+        navigationOptions:({navigation})=>({
+            title:`${navigation.state.params.name}页面名`
+
+        })
+    },
+
+
+    Page2:{// 静态
+        screen:Page2,
+        navigationOptions:{
+            title:'This is Page2'
+        }
+
+    },
+
+
+    Page3:{
+        screen:Page3,
+        navigationOptions:(props)=>{
+            const {navigation}=props;
+            const {state,setParams}=navigation;
+            const {params}=state;
+
+            return {
+                title:params.title?params.title:'This is Page3',
+                headerRight:(
+                    <Button
+                        title={params.mode==='edit'?'保存':'编辑'}
+                        onPress={()=>
+                            setParams({mode:params.mode==='edit'?'':'edit'})
+                        }
+                    >
+                    </Button>
+                )
+            }
+        }
+
+    },
+
+
+
+
+})
--- RNProjectDemo/index.js
+++ RNProjectDemo/index.js
@@ -6,4 +6,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import {createAppContainer} from 'react-navigation';
+import {AppStackNavigator} from "./Section/Navigators/AppNavigators";
+
+const AppStackNavigatorContainer = createAppContainer(AppStackNavigator)
+
+AppRegistry.registerComponent(appName, () => AppStackNavigatorContainer);
